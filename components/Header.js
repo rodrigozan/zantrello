dddb94f4af99ae27f66ff32e54941bd64646e1d1
Rodrigo Zan <rodzandonadi@gmail.com>
2023-09-28 13:43:56 +0000

    Created Header and Footer code

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-blue-500 py-4 text-white text-center">
+      <h1 className="text-2xl font-semibold">Meu Aplicativo Trello</h1>
+    </header>
+  );
+};
+
+export default Header;
